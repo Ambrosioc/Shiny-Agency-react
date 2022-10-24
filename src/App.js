6d001b9a9b8712c438e583bd2d3daaa331d0a35d
Ambrosie Cazimira <a.cazimira@gmail.com>
2022-10-24 03:23:31 +0200

    add page results in this app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import NotFound from "./Components/error/NotFound";
 import Footer from "./Components/footer/Footer";
 import Nav from "./Components/Header/Nav";
-import { Home, Survey } from "./pages";
+import { Home, Results, Survey } from "./pages";
 import Freelances from "./pages/freelances/Freelances";
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/survey/:questionNumber" element={<Survey />} />
-        <Route path="/results" element={<div>Resultats</div>} />
+        <Route path="/results" element={<Results />} />
         <Route path="/freelances" element={<Freelances />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
